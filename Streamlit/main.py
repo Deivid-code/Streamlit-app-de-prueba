f66ed3a68153e2a29b5d64e43a02761c404f3117
Deivid-code <71236057+Deivid-code@users.noreply.github.com>
2023-02-12 13:01:45 -0500

    ...

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 table = pd.DataFrame({"Column 1": [1,2,3,4,5,6,7],
 "Column 2": [11,12,13,14,15,16,17]})
@@ -32,8 +33,11 @@ st.metric(label = "Wind Speed", value="120ms⁻¹", delta = "-1.4ms⁻¹")
 st.table(table)
 st.dataframe(table)
 
-st.image("C:/Users/David/Documents/GitHub/Streamlit-app-de-prueba/Streamlit/mapa colombia.jpg",
-caption="Esta es una imagen de la bandera de Colombia", width=500)
+#st.image("C:/Users/David/Documents\GitHub/Streamlit-app-de-prueba/Streamlit/bandera Colombia.jpg",
+#caption="Esta es una imagen de la bandera de Colombia", width=500)
+
+print(Image.open('Streamlit/bandera Colombia.jpg'))
+
 
 video_url = "https://www.youtube.com/embed/LxsMdHrNS-o"
 st.markdown(f'<iframe width="560" height="315" src="{video_url}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
